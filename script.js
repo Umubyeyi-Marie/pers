98dc5c3d1c9cdb731eb113fc4d316bb75ab5e7a5
Umubyeyi-Marie <mariebienvenueu@gmail.com>
2023-12-13 06:37:26 +0200

    second push

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ window.addEventListener('scroll',function(){
     const scrollPos = window.scrollY
     sections.forEach(section =>{
         if(scrollPos > section.offsetTop&& scrollPos <(section.offsetTop +section.offsetHeight)){
-            navLinks.forEach(link =>
+            navLinks.forEach(link =>{
                 link.classList.remove("active");
                 if(section.getAttribute('id')===link.getAttribute('href').substring(1)){
                     link.classList.add("active")
